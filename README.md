fba5be95e2b3da69b2fe9282df6f7c4ed956bf3f
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-10-05 16:40:13 -0300

    add some endpoints

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tech_maromba
+
 Project done for an embedded system to be used at gyms
+
+## Server
+
+To run the server, go to its folder, then:
+`$ export FLASK_APP=hello`
+`$ export FLASK_ENV=development`
+`$ flask run`
--- server/hello.py
+++ server/hello.py
@@ -8,19 +8,49 @@ def get_db_connection():
 
 app = Flask(__name__)
 
+QUEUE_ELEMENT_INDEXES = {
+  'id': 0,
+  'inserted_at': 1,
+  'machine_id': 2,
+  'user_id': 3,
+  'series': 4,
+  'repetition': 5,
+  'status': 6,
+}
+
 @app.route('/')
 def index():
   return "Olá, maromba."
 
 @app.route('/queue/<int:machine_id>')
 def queue(machine_id):
-  conn = get_db_connection()
-  queue = conn.execute('SELECT * FROM queue_elements WHERE machine_id = ?', (machine_id,)).fetchall()
-  return json.dumps(queue)
+  return get_queue_for_machine(machine_id)
 
 @app.route('/machines')
 def machines():
   conn = get_db_connection()
   queue = conn.execute('SELECT * FROM machines').fetchall()
   return json.dumps(queue)
+
+@app.route('/queue/<int:machine_id>/<int:user_id>')
+def position_in_queue(machine_id, user_id):
+  conn = get_db_connection()
+  queue = get_queue_for_machine(machine_id)
+  sorted_queue = queue.sort(key=key_for_sorting_queue).index(user_id)
+  user_id_queue = list(map(lambda x: x[QUEUE_ELEMENT_INDEXES['user_id']], sorted_queue))
+  return user_id_queue.index(user_id) + 1
+
+@app.route('/queue/insert/<int:machine_id>/<int:user_id>/<int:series>/<int:repetitions>')
+def insert_in_queue(machine_id, user_id):
+  conn = get_db_connection()
+  inserted = conn.execute('INSERT INTO queue_elements(machine_id, user_id, series, repetitions) VALUES(machine_id, user_id, series, repetitions)')
+
+def key_for_sorting_queue(queue_element):
+  return queue_element[QUEUE_ELEMENT_INDEXES['inserted_at']]
+
+def get_queue_for_machine(machine_id):
+  conn = get_db_connection()
+  queue = conn.execute('SELECT * FROM queue_elements WHERE machine_id = ? AND status = \'WAITING\'', (machine_id,)).fetchall()
+  return json.dumps(queue)
+
 
