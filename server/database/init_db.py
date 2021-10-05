57864420607cabfbbafedbbec369641949006165
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-10-05 17:12:37 -0300

    add more base data in init db

--- server/database/init_db.py
+++ server/database/init_db.py
@@ -10,9 +10,36 @@ cur = connection.cursor()
 cur.execute("INSERT INTO machines (name) VALUES (?)",
             ('Cadeira extensora',)
             )
+cur.execute("INSERT INTO machines (name) VALUES (?)",
+            ('Cadeira flexora',)
+            )
+cur.execute("INSERT INTO machines (name) VALUES (?)",
+            ('Cadeira abdutora',)
+            )
+cur.execute("INSERT INTO machines (name) VALUES (?)",
+            ('Cadeira adutora',)
+            )
+cur.execute("INSERT INTO machines (name) VALUES (?)",
+            ('Leg press',)
+            )
+cur.execute("INSERT INTO machines (name) VALUES (?)",
+            ('Pulley',)
+            )
 
 cur.execute("INSERT INTO users (name) VALUES (?)",
             ('Larry Lagosta',)
+            )
+cur.execute("INSERT INTO users (name) VALUES (?)",
+            ('Dobby Maromba',)
+            )
+cur.execute("INSERT INTO users (name) VALUES (?)",
+            ('Emoji Maromba',)
+            )
+cur.execute("INSERT INTO users (name) VALUES (?)",
+            ('Moana Maromba',)
+            )
+cur.execute("INSERT INTO users (name) VALUES (?)",
+            ('Tuk Maromba',)
             )
 
 cur.execute("INSERT INTO queue_elements (machine_id, user_id, series, repetitions) VALUES (?, ?, ?, ?)",
