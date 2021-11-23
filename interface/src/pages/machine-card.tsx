168d66c21beacedca6a2f0e305c33e86f3fd0653
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-11-23 17:39:53 -0300

    allow user to insert in queue

--- interface/src/pages/machine-card.tsx
+++ interface/src/pages/machine-card.tsx
@@ -1,6 +1,8 @@
-import { Button, Card } from "antd";
-import React, { Fragment, useEffect, useState } from "react";
-import { getQueue } from "../utils/endpoints";
+import { Button, Card, InputNumber, Modal, Space } from "antd";
+import { valueType } from "antd/lib/statistic/utils";
+import React, { useEffect, useState } from "react";
+import { CONSTANTS } from "../utils/constants";
+import { getQueue, insertInQueue } from "../utils/endpoints";
 
 interface IMachineCard {
   name: string;
@@ -9,12 +11,51 @@ interface IMachineCard {
 
 const MachineCard: React.FC<IMachineCard> = ({ id, name }) => {
   const [queue, setQueue] = useState<any>();
+  const [userQueuePosition, setUserQueuePosition] = useState<any>();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [seriesNumber, setSeriesNumber] = useState<number>();
+  const [repetitionsNumber, setRepetitionsNumber] = useState<number>();
 
   useEffect(() => {
     getQueue(id).then((res) => {
       setQueue(res);
+
+      const position = res.findIndex((q: any) => q[3] === CONSTANTS.userId);
+      if (position !== -1) {
+        setUserQueuePosition(position + 1);
+        console.log(position);
+      }
     });
   }, []);
+
+  const updateQueue = () => {
+    getQueue(id).then((res) => {
+      setQueue(res);
+
+      const position = res.findIndex((q: any) => q[3] === CONSTANTS.userId);
+      if (position) {
+        setUserQueuePosition(position + 1);
+      }
+    });
+  };
+
+  const handleOk = () => {
+    if (!seriesNumber || !repetitionsNumber) {
+      alert("Preencha todos os campos!");
+    } else {
+      insertInQueue(id, CONSTANTS.userId, seriesNumber, repetitionsNumber).then(
+        (res) => {
+          if (res.result.includes("Success")) {
+            alert("Inserido na fila!");
+            updateQueue();
+            setIsModalVisible(false);
+          } else {
+            alert("Ops, algo deu errado. Tente novamente.");
+          }
+        }
+      );
+    }
+  };
 
   return (
     <Card
@@ -22,8 +63,32 @@ const MachineCard: React.FC<IMachineCard> = ({ id, name }) => {
       bordered
       style={{ width: 300, border: "1px solid black", marginBottom: 10 }}
     >
-      <Button type="primary">Se inserir na fila</Button>
+      <Button type="primary" onClick={() => setIsModalVisible(true)}>
+        Se inserir na fila
+      </Button>
+      <Modal
+        title={`Fila - ${name}`}
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        onOk={handleOk}
+      >
+        Digite as séries e repetições:
+        <Space style={{ marginLeft: "8px" }}>
+          <InputNumber
+            placeholder="4"
+            value={seriesNumber as valueType}
+            onChange={(value) => setSeriesNumber(value as number)}
+          />
+          x
+          <InputNumber
+            placeholder="15"
+            value={repetitionsNumber as valueType}
+            onChange={(value) => setRepetitionsNumber(value as number)}
+          />
+        </Space>
+      </Modal>
       <div>Há {queue?.length} pessoas na fila.</div>
+      {userQueuePosition > 0 && <div>Sua posição é a {userQueuePosition}.</div>}
     </Card>
   );
 };
---
+++ interface/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const CONSTANTS = {
+  userId: 1,
+};
